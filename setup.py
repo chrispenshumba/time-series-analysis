956c1d18a91954f8633438cc91083c22e1726e7e
chrispen <chrispenshumba@icloud.com>
2020-07-21 01:33:41 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is a time series project on weather data at the Mother Cap point at the peak district.',
+    author='chrispen',
+    license='MIT',
+)
